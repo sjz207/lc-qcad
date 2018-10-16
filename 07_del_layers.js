68a73608781f5ea4b9836d7b117d24a7eef8cf0b
Ronald Römer <rroemer@gmail.com>
2018-10-16 08:33:31 +0200

    Markers and other changes 2

---
+++ 07_del_layers.js
@@ -0,0 +1,35 @@
+/* Copyright (c) 2018, Ronald Römer
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+include('/home/zippy/lc-qcad/tools.js');
+
+var cfg = JSON.parse(readTextFile('/home/zippy/lc-qcad/cfg.json'));
+
+(function() {
+    var before = Date.now();
+
+    var doc = getDocument();
+    var di = getDocumentInterface();
+
+    if (cfg['del-tmp-layers']) {
+        var layers = ['Convex', 'New', 'OBB', 'Offs'];
+
+        if (!cfg['add-markers']) {
+            layers.push('Markers');
+        }
+
+        var op = new RDeleteObjectsOperation(false);
+
+        layers.forEach(function (name) {
+            var lay = doc.queryLayer(name);
+            op.deleteObject(lay);
+        });
+
+        di.applyOperation(op);
+    }
+
+    qDebug((Date.now()-before)/1e3, 's');
+
+})();
